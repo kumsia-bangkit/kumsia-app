5de0151aa27056e0075df2257043a9abaa40ea56
trisnobp <trisnotyo6@gmail.com>
2024-06-09 22:04:57 +0700

    feat: social events draft page + fragment adjustment

--- app/src/main/java/com/dicoding/kumsiaapp/view/organization/events/EventsFragment.kt
+++ app/src/main/java/com/dicoding/kumsiaapp/view/organization/events/EventsFragment.kt
@@ -5,56 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.data.remote.response.Event
+import com.dicoding.kumsiaapp.databinding.FragmentEventsBinding
+import com.dicoding.kumsiaapp.databinding.FragmentHomeBinding
+import com.dicoding.kumsiaapp.view.organization.utils.EventAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EventsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EventsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentEventsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false)
+    ): View {
+        _binding = FragmentEventsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EventsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EventsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listOfEvent = mutableListOf(
+            Event("Bincang-Bincang Masak", "12 Oktober 2023", "Memasak"),
+            Event("Catur Bersama", "12 Oktober 2023", "Olahraga"),
+            Event("Bincang-Bincang Masak", "12 Oktober 2023", "Memasak"),
+            Event("Catur Bersama", "12 Oktober 2023", "Olahraga"),
+        )
+
+        val layoutManager = LinearLayoutManager(requireActivity())
+        binding.rvEvents.layoutManager = layoutManager
+
+        val adapter = EventAdapter()
+        adapter.submitList(listOfEvent)
+        binding.rvEvents.adapter = adapter
     }
 }
