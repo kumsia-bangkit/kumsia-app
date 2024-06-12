1ed29afb46fef52acb47ee21722a853564648428
trisnobp <trisnotyo6@gmail.com>
2024-06-12 20:46:48 +0700

    feat: add event page + preferences page

--- app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/AddEventActivity.kt
+++ app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/AddEventActivity.kt
@@ -1,22 +1,45 @@
 package com.dicoding.kumsiaapp.view.organization.event
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.kumsiaapp.R
 import com.dicoding.kumsiaapp.databinding.ActivityAddEventBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
 
 class AddEventActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddEventBinding
+    private lateinit var eventDateAndTime: String
+    private lateinit var city: String
+    private lateinit var eventStatus: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityAddEventBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(com.dicoding.kumsiaapp.R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
@@ -26,6 +49,108 @@ class AddEventActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.dateTimeButton.setOnClickListener {
+            pickDateTime()
+        }
 
+        binding.radioGroup.setOnCheckedChangeListener {_, checkedId ->
+            if (checkedId == binding.online.id) {
+                eventStatus = "Online"
+                binding.offline.isChecked = false
+                binding.edLocation.visibility = View.GONE
+                binding.locationTitle.visibility = View.GONE
+                binding.citySpinner.visibility = View.GONE
+                binding.citySpinner.visibility = View.GONE
+                binding.cityTitle.visibility = View.GONE
+                binding.edEventLink.visibility = View.VISIBLE
+                binding.eventLinkTitle.visibility = View.VISIBLE
+            } else {
+                eventStatus = "Offline"
+                binding.online.isChecked = false
+                binding.edLocation.visibility = View.VISIBLE
+                binding.locationTitle.visibility = View.VISIBLE
+                binding.citySpinner.visibility = View.VISIBLE
+                binding.citySpinner.visibility = View.VISIBLE
+                binding.cityTitle.visibility = View.VISIBLE
+                binding.edEventLink.visibility = View.GONE
+                binding.eventLinkTitle.visibility = View.GONE
+            }
+        }
+
+        binding.citySpinner.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(
+                R.layout.dialog_searchable_spinner)
+            dialog.window?.setLayout(1000, 800)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.show()
+
+            // Initialize and assign variable
+            val editText: EditText = dialog.findViewById(R.id.edit_text)
+            val listView: ListView = dialog.findViewById(R.id.list_view)
+
+            val adapter: ArrayAdapter<Any?> = ArrayAdapter<Any?>(
+                this,
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.courses_array)
+            )
+
+
+            listView.adapter = adapter
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    adapter.filter.filter(s)
+                }
+
+                override fun afterTextChanged(s: Editable) {
+                }
+            })
+
+            listView.onItemClickListener =
+                OnItemClickListener { _, _, position, _ -> // when item selected from list
+                    binding.citySpinner.text = adapter.getItem(position).toString()
+                    city = adapter.getItem(position).toString()
+                    dialog.dismiss()
+                }
+        }
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, AddEventPreferencesActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun pickDateTime() {
+        val currentDateTime = Calendar.getInstance()
+        val startYear = currentDateTime.get(Calendar.YEAR)
+        val startMonth = currentDateTime.get(Calendar.MONTH)
+        val startDay = currentDateTime.get(Calendar.DAY_OF_MONTH)
+        val startHour = currentDateTime.get(Calendar.HOUR_OF_DAY)
+        val startMinute = currentDateTime.get(Calendar.MINUTE)
+
+        DatePickerDialog(this, { _, year, month, day ->
+            TimePickerDialog(this, { _, hour, minute ->
+                val pickedDateTime = Calendar.getInstance()
+                pickedDateTime.set(year, month, day, hour, minute)
+
+                val timestamp = pickedDateTime.timeInMillis
+                val textFormat = SimpleDateFormat("dd MMMM yyyy, HH:mm", Locale.getDefault())
+                val date = textFormat.format(timestamp)
+                binding.edDatetime.text = SpannableStringBuilder(date)
+
+                val isoDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+                isoDateFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+                eventDateAndTime = isoDateFormat.format(Date(timestamp))
+            }, startHour, startMinute, android.text.format.DateFormat.is24HourFormat(this)).show()
+        }, startYear, startMonth, startDay).show()
     }
 }
---
+++ app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/AddEventPreferencesActivity.kt
@@ -0,0 +1,185 @@
+package com.dicoding.kumsiaapp.view.organization.event
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.databinding.ActivityAddEventPreferencesBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
+class AddEventPreferencesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddEventPreferencesBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddEventPreferencesBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+        binding.addCityChip.setOnClickListener {
+            addCityDialog()
+        }
+
+        addGenderChips()
+        addReligionChips()
+        addInterestsChips()
+    }
+
+    private fun addGenderChips() {
+        val interestsArray = resources.getStringArray(R.array.gender_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventGenderGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addReligionChips() {
+        val interestsArray = resources.getStringArray(R.array.religion_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventReligionGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addInterestsChips() {
+        val interestsArray = resources.getStringArray(R.array.interests_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventInterestGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addCityDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(
+            R.layout.dialog_searchable_spinner)
+        dialog.window?.setLayout(1000, 800)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        // Initialize and assign variable
+        val editText: EditText = dialog.findViewById(R.id.edit_text)
+        val listView: ListView = dialog.findViewById(R.id.list_view)
+
+        val adapter: ArrayAdapter<Any?> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.courses_array)
+        )
+
+
+        listView.adapter = adapter
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        listView.onItemClickListener =
+            OnItemClickListener { _, _, position, _ ->
+                addNewChip(adapter.getItem(position).toString())
+                dialog.dismiss()
+            }
+
+    }
+
+    private fun addNewChip(chipText: String) {
+        val chipGroup = binding.eventCityGroup
+        val chip =
+            layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+        chip.text = chipText
+        chip.id = View.generateViewId()
+        chip.isClickable = false
+        chip.isCheckable = true
+        chip.isChecked = true
+        chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        chip.layoutParams = ChipGroup.LayoutParams(
+            ChipGroup.LayoutParams.WRAP_CONTENT,
+            ChipGroup.LayoutParams.WRAP_CONTENT
+        )
+        chip.isCloseIconVisible = true
+        chip.setCloseIconResource(R.drawable.baseline_close_24)
+        chip.setOnCloseIconClickListener {
+            chipGroup.removeView(chip)
+        }
+
+        chipGroup.addView(chip, 0)
+    }
+}
