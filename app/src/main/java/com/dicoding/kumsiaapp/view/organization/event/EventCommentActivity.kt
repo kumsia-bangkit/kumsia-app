28d685ec94e0dc228ad1ece2478a46928a9d1f23
trisnobp <trisnotyo6@gmail.com>
2024-06-13 19:02:48 +0700

    feat: add comment page + card for adapter

---
+++ app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/EventCommentActivity.kt
@@ -0,0 +1,29 @@
+package com.dicoding.kumsiaapp.view.organization.event
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.databinding.ActivityEventCommentBinding
+
+class EventCommentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEventCommentBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEventCommentBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+    }
+}
