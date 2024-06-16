3a700a6e2dba9a22e3bb034fddef2bd3279c6679
trisnobp <trisnotyo6@gmail.com>
2024-06-16 21:06:31 +0700

    feat: event detail with whatsapp intent

--- app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/OrganizationDetailEventActivity.kt
+++ app/src/main/java/com/dicoding/kumsiaapp/view/organization/event/OrganizationDetailEventActivity.kt
@@ -1,13 +1,27 @@
 package com.dicoding.kumsiaapp.view.organization.event
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.data.local.UserPreferences
+import com.dicoding.kumsiaapp.data.local.dataStore
+import com.dicoding.kumsiaapp.data.remote.response.EventsItem
 import com.dicoding.kumsiaapp.databinding.ActivityOrganizationDetailEventBinding
+import com.dicoding.kumsiaapp.utils.DateFormatter
+import com.dicoding.kumsiaapp.viewmodel.SessionViewModel
+import com.dicoding.kumsiaapp.viewmodel.SessionViewModelFactory
+
 
 class OrganizationDetailEventActivity : AppCompatActivity() {
 
@@ -23,6 +37,21 @@ class OrganizationDetailEventActivity : AppCompatActivity() {
             insets
         }
 
+        val pref = UserPreferences.getInstance(application.dataStore)
+        val sessionViewModel = ViewModelProvider(this, SessionViewModelFactory(pref))[SessionViewModel::class.java]
+        sessionViewModel.getUserName().observe(this) {
+            if (it != null) {
+                binding.tvOrganizer.text = it
+            }
+        }
+
+        val eventData = intent.getParcelableExtra<EventsItem>(EVENT_DATA)
+        setEventData(eventData)
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
         binding.cancelButton.setOnClickListener {
             val builder = AlertDialog.Builder(this)
             builder.setMessage("Are you sure you want to cancel this event?")
@@ -36,6 +65,72 @@ class OrganizationDetailEventActivity : AppCompatActivity() {
             val alert = builder.create()
             alert.show()
         }
+    }
+
+    private fun setEventData(data: EventsItem?) {
+        Glide.with(this).load(data?.profilePicture).into(binding.eventImage)
+        binding.eventTitle.text = data?.name
+        binding.tvItemDate.text = DateFormatter.formatDate(data?.eventStart!!)
+        binding.eventType.text = data.type
+        if (data.type == "Offline") {
+            binding.eventType.chipBackgroundColor = ColorStateList.valueOf(ContextCompat.getColor(baseContext, R.color.grey))
+            binding.tvLocation.text = buildString {
+                append(data.location)
+                append(", ")
+                append(data.city)
+            }
+        } else {
+            binding.eventType.chipBackgroundColor = ColorStateList.valueOf(
+                ContextCompat.getColor(
+                    baseContext,
+                    android.R.color.holo_green_dark
+                )
+            )
+            binding.tvLocationTitle.text = resources.getString(R.string.meeting_link)
+            binding.tvLocation.text = data.link
+        }
+
+        binding.tvCapacityValue.text = data.capacity.toString()
+        binding.tvDescription.text = data.description
+        binding.tvAttendee.text = data.attendeeCriteria
+
+        binding.tvGenderPreferences.text = stringOfPreferences(data.genderPreference)
+        binding.tvReligionPreferences.text = stringOfPreferences(data.religionPreference)
+        binding.tvInterestsPreferences.text = stringOfPreferences(data.hobbyPreference)
+        binding.tvCityPreferences.text = stringOfPreferences(data.cityPreference)
+
+        if (isWhatsAppLink(data.contactVarchar!!)) {
+            binding.tvContact.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setData(Uri.parse(data.contactVarchar))
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "WhatsApp is not installed", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            binding.tvContact.text = data.contactVarchar
+        }
+    }
+
+    private fun stringOfPreferences(data: List<String?>?): String {
+        val preferences = StringBuilder()
+        if (data!!.isEmpty()) return "-"
+
+        for (i in data.indices) {
+            preferences.append(data[i])
+            if (i < data.size - 1) {
+                preferences.append(", ")
+            }
+        }
+
+        return preferences.toString()
+    }
+
+    private fun isWhatsAppLink(link: String): Boolean {
+        val regex = "^https://chat\\.whatsapp\\.com/.+$"
+        return link.matches(regex.toRegex())
     }
 
     companion object {
