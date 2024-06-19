3eca499e9e1402abd244841a1a925eae9992acce
trisnobp <trisnotyo6@gmail.com>
2024-06-19 14:14:25 +0700

    feat: update user profile with preferences

--- app/src/main/java/com/dicoding/kumsiaapp/view/individual/profile/EditIndividualProfileActivity.kt
+++ app/src/main/java/com/dicoding/kumsiaapp/view/individual/profile/EditIndividualProfileActivity.kt
@@ -1,19 +1,46 @@
 package com.dicoding.kumsiaapp.view.individual.profile
 
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.view.View
+import android.view.WindowManager
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.data.remote.request.UpdateUserProfileDTO
 import com.dicoding.kumsiaapp.data.remote.response.UserDTO
 import com.dicoding.kumsiaapp.databinding.ActivityEditIndividualProfileBinding
+import com.dicoding.kumsiaapp.utils.DateFormatter
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class EditIndividualProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditIndividualProfileBinding
     private var currentImageUri: Uri? = null
+    private lateinit var dateOfBirth: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +52,262 @@ class EditIndividualProfileActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        this.window.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN
+        )
 
         val userData = intent.getParcelableExtra<UserDTO>(USER_DATA)
         setUserData(userData)
+
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+        binding.citySpinner.setOnClickListener {
+            showCityDialog()
+        }
+
+        binding.dobButton.setOnClickListener {
+            pickDate()
+        }
+
+        binding.religionSpinner.setOnClickListener {
+            showDialog("Religion")
+        }
+
+        binding.citySpinner.setOnClickListener {
+            showDialog("City")
+        }
+
+        binding.addPhotoButton.setOnClickListener {
+            startGallery()
+        }
+
+        binding.nextButton.setOnClickListener {
+            val nameField = binding.edRegisterName.text.toString().trim()
+            val usernameField = binding.edRegisterUsername.text.toString().trim()
+            val emailField = binding.edRegisterEmail.text.toString().trim()
+            val genderField = binding.genderSpinner.text.toString().trim()
+            val dobField = binding.edDob.text.toString().trim()
+            val oldPasswordField = binding.edConfigmEditPassword.text.toString().trim()
+
+            // Optional fields
+            val newPasswordField = binding.edEditPassword.text.toString().trim()
+            val religionField = binding.religionSpinner.text.toString().trim()
+            val cityField = binding.citySpinner.text.toString().trim()
+            val contact = binding.edContact.text.toString().trim()
+            val guardianContact = binding.edGuardianContact.text.toString().trim()
+
+            if (nameField.isEmpty() || usernameField.isEmpty() || emailField.isEmpty() || genderField.isEmpty() ||
+                dobField.isEmpty() || oldPasswordField.isEmpty()) {
+                    binding.errorMessage.let {
+                        it.text = getString(R.string.required_fields_can_t_be_empty)
+                        it.visibility = View.VISIBLE
+                    }
+                }
+            else {
+                binding.errorMessage.visibility = View.GONE
+                val userDTO = UpdateUserProfileDTO(
+                    username = usernameField,
+                    name = nameField,
+                    email = emailField,
+                    contact = contact.ifEmpty { null },
+                    guardianContact = guardianContact.ifEmpty { null },
+                    religion = religionField.ifEmpty { null },
+                    gender = genderField,
+                    dob = dateOfBirth,
+                    city = cityField,
+                    newPassword = newPasswordField,
+                    password = oldPasswordField,
+                    genderPreference = userData?.user?.preferenceGender,
+                    religionPreference = userData?.user?.preferenceReligion,
+                    cityPreference = userData?.user?.preferenceCity,
+                    hobbyPreference = userData?.user?.preferenceHobby,
+                )
+
+                val intent = Intent(this, EditPreferencesActivity::class.java)
+                intent.putExtra(EditPreferencesActivity.USER_DATA, userDTO)
+                intent.putExtra(EditPreferencesActivity.IMAGE_FILE, currentImageUri)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun pickDate() {
+        val currentDateTime = Calendar.getInstance()
+        val startYear = currentDateTime.get(Calendar.YEAR)
+        val startMonth = currentDateTime.get(Calendar.MONTH)
+        val startDay = currentDateTime.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, { _, year, month, day ->
+            val pickedDateTime = Calendar.getInstance()
+            pickedDateTime.set(year, month, day)
+
+            val timestamp = pickedDateTime.timeInMillis
+            val textFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+            val date = textFormat.format(timestamp)
+            binding.edDob.text = SpannableStringBuilder(date)
+
+            val dataFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+            dateOfBirth = dataFormat.format(Date(timestamp))
+        }, startYear, startMonth, startDay).show()
+    }
+
+    private fun showDialog(content: String) {
+        val dialog = Dialog(this)
+        dialog.setContentView(
+            R.layout.dialog_searchable_spinner)
+        dialog.window?.setLayout(1000, 800)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        // Initialize and assign variable
+        val editText: EditText = dialog.findViewById(R.id.edit_text)
+        val listView: ListView = dialog.findViewById(R.id.list_view)
+        val adapter: ArrayAdapter<Any?> = if (content == "City") {
+            ArrayAdapter<Any?>(
+                this,
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.city_array)
+            )
+        } else {
+            ArrayAdapter<Any?>(
+                this,
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.religion_array)
+            )
+        }
+
+
+        listView.adapter = adapter
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        listView.onItemClickListener =
+            OnItemClickListener { _, _, position, _ ->
+                if (content == "City") {
+                    binding.citySpinner.text = adapter.getItem(position).toString()
+                } else {
+                    binding.religionSpinner.text = adapter.getItem(position).toString()
+                }
+                dialog.dismiss()
+            }
+    }
+
+    private fun showCityDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(
+            R.layout.dialog_searchable_spinner)
+        dialog.window?.setLayout(1000, 800)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        // Initialize and assign variable
+        val editText: EditText = dialog.findViewById(R.id.edit_text)
+        val listView: ListView = dialog.findViewById(R.id.list_view)
+
+        val adapter: ArrayAdapter<Any?> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.city_array)
+        )
+
+
+        listView.adapter = adapter
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        listView.onItemClickListener =
+            OnItemClickListener { _, _, position, _ -> // when item selected from list
+                binding.citySpinner.text = adapter.getItem(position).toString()
+                dialog.dismiss()
+            }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            showToast(getString(R.string.no_media_is_selected))
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            binding.profileChange.setImageURI(it)
+        }
     }
 
     private fun setUserData(userData: UserDTO?) {
+        dateOfBirth = userData?.user?.dob!!
 
+        if (userData.user.profilePicture != null) {
+            Glide.with(this)
+                .load(userData.user.profilePicture)
+                .into(binding.profileChange)
+        }
+
+        binding.apply {
+            edRegisterName.text = SpannableStringBuilder(userData.user.name)
+            edRegisterUsername.text = SpannableStringBuilder(userData.user.username)
+            edRegisterEmail.text = SpannableStringBuilder(userData.user.email)
+            genderSpinner.text = SpannableStringBuilder(userData.user.gender)
+            edDob.text = SpannableStringBuilder(DateFormatter.formatDOB(userData.user.dob))
+
+            // Optional field (need to check null values)
+            if (!userData.user.religion.isNullOrEmpty()) {
+                religionSpinner.text = SpannableStringBuilder(userData.user.religion)
+            }
+            if (!userData.user.city.isNullOrEmpty()) {
+                citySpinner.text = SpannableStringBuilder(userData.user.city)
+            }
+            if (!userData.user.contact.isNullOrEmpty()) {
+                edContact.text = SpannableStringBuilder(userData.user.contact)
+            }
+            if (!userData.user.guardianContact.isNullOrEmpty()) {
+                edGuardianContact.text = SpannableStringBuilder(userData.user.guardianContact)
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/com/dicoding/kumsiaapp/view/individual/profile/EditPreferencesActivity.kt
+++ app/src/main/java/com/dicoding/kumsiaapp/view/individual/profile/EditPreferencesActivity.kt
@@ -1,16 +1,58 @@
 package com.dicoding.kumsiaapp.view.individual.profile
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.kumsiaapp.R
+import com.dicoding.kumsiaapp.data.local.UserPreferences
+import com.dicoding.kumsiaapp.data.local.dataStore
+import com.dicoding.kumsiaapp.data.remote.request.UpdateUserProfileDTO
 import com.dicoding.kumsiaapp.databinding.ActivityEditPreferencesBinding
+import com.dicoding.kumsiaapp.utils.JwtDecoder
+import com.dicoding.kumsiaapp.utils.reduceFileImage
+import com.dicoding.kumsiaapp.utils.uriToFile
+import com.dicoding.kumsiaapp.view.individual.IndividualActivity
+import com.dicoding.kumsiaapp.viewmodel.ProfileViewModel
+import com.dicoding.kumsiaapp.viewmodel.SessionViewModel
+import com.dicoding.kumsiaapp.viewmodel.SessionViewModelFactory
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.google.gson.Gson
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 
 class EditPreferencesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditPreferencesBinding
+    private val profileViewModel: ProfileViewModel by lazy {
+        ViewModelProvider(this)[ProfileViewModel::class.java]
+    }
+    private val listOfGender = mutableListOf<String>()
+    private val listOfReligion = mutableListOf<String>()
+    private val listOfInterests = mutableListOf<String>()
+    private val listOfCity = mutableListOf<String>()
+    private lateinit var token: String
+
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,5 +63,270 @@ class EditPreferencesActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val userData = intent.getParcelableExtra<UpdateUserProfileDTO>(USER_DATA)
+        setPreferencesData(userData)
+
+        val pref = UserPreferences.getInstance(application.dataStore)
+        val sessionViewModel = ViewModelProvider(this, SessionViewModelFactory(pref))[SessionViewModel::class.java]
+
+        sessionViewModel.getUserToken().observe(this) {
+            token = it!!
+        }
+
+        profileViewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
+        profileViewModel.tokenData.observe(this) {
+            it.getContentIfNotHandled()?.let { data ->
+                val claims = JwtDecoder.decode(data.accessToken!!)
+                val name = claims.getClaim("name").asString()
+                val role = claims.getClaim("role").asString()
+                val isNewUser = claims.getClaim("is_new_user").asBoolean() ?: true
+                sessionViewModel.saveSession(data.accessToken, name!!, role!!, isNewUser)
+
+                val intent = Intent(this, IndividualActivity::class.java)
+                intent.putExtra(IndividualActivity.FRAGMENT_POSITION, 3)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+                finish()
+
+                showToast("Data is successfully updated!")
+            } ?: run {
+                showToast("Failed to update user data!")
+            }
+        }
+
+        binding.eventGenderGroup.setOnCheckedStateChangeListener{ chipGroup, checkedIds ->
+            listOfGender.clear()
+            checkedIds.forEach {
+                val chip = chipGroup.findViewById<Chip>(it)
+                listOfGender.add(chip.text.toString())
+            }
+        }
+
+        binding.eventReligionGroup.setOnCheckedStateChangeListener { chipGroup, checkedIds ->
+            listOfReligion.clear()
+            checkedIds.forEach {
+                val chip = chipGroup.findViewById<Chip>(it)
+                listOfReligion.add(chip.text.toString())
+            }
+        }
+
+        binding.eventInterestGroup.setOnCheckedStateChangeListener { chipGroup, checkedIds ->
+            listOfInterests.clear()
+            checkedIds.forEach {
+                val chip = chipGroup.findViewById<Chip>(it)
+                listOfInterests.add(chip.text.toString())
+            }
+        }
+
+        binding.addCityChip.setOnClickListener {
+            addCityDialog()
+        }
+
+        binding.saveButton.setOnClickListener {
+            val imageUri = intent.getParcelableExtra<Uri>(IMAGE_FILE)
+            val imageMultipart: MultipartBody.Part?
+
+            userData?.genderPreference = listOfGender
+            userData?.religionPreference = listOfReligion
+            userData?.hobbyPreference = listOfInterests
+            userData?.cityPreference = listOfCity
+
+            // Convert event data into json format
+            val gson = Gson()
+            val jsonData = gson.toJson(userData)
+            val requestBody = jsonData.toRequestBody("text/plain".toMediaType())
+
+            if (imageUri == null) {
+                profileViewModel.updateUserProfile(token, requestBody, null)
+            } else {
+                val imageFile = uriToFile(imageUri, this).reduceFileImage()
+                val requestImageFile = imageFile.asRequestBody("image/jpeg".toMediaType())
+                imageMultipart = MultipartBody.Part.createFormData(
+                    "file",
+                    imageFile.name,
+                    requestImageFile
+                )
+                profileViewModel.updateUserProfile(token, requestBody, imageMultipart)
+            }
+
+        }
+    }
+
+    private fun setPreferencesData(userData: UpdateUserProfileDTO?) {
+        userData?.genderPreference?.forEach {
+            listOfGender.add(it!!)
+        }
+
+        userData?.religionPreference?.forEach {
+            listOfReligion.add(it!!)
+        }
+
+        userData?.hobbyPreference?.forEach {
+            listOfInterests.add(it!!)
+        }
+
+        userData?.cityPreference?.forEach {
+            addNewChip(it!!)
+        }
+
+        addGenderChips(listOfGender)
+        addReligionChips(listOfReligion)
+        addInterestsChips(listOfInterests)
+    }
+
+    private fun addGenderChips(list: List<String?>?) {
+        val interestsArray = resources.getStringArray(R.array.gender_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventGenderGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chip.isChecked = list?.contains(interest)!!
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addReligionChips(list: List<String?>?) {
+        val interestsArray = resources.getStringArray(R.array.religion_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventReligionGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chip.isChecked = list?.contains(interest)!!
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addInterestsChips(list: List<String?>?) {
+        val interestsArray = resources.getStringArray(R.array.interests_array)
+        val interestsList = interestsArray.toList()
+        val chipGroup = binding.eventInterestGroup
+
+        interestsList.forEachIndexed { index, interest ->
+            val chip =
+                layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+            chip.text = interest
+            chip.id = index
+            chip.isClickable = true
+            chip.isCheckable = true
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            chip.layoutParams = ChipGroup.LayoutParams(
+                ChipGroup.LayoutParams.WRAP_CONTENT,
+                ChipGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            chip.isChecked = list?.contains(interest)!!
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun addCityDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(
+            R.layout.dialog_searchable_spinner)
+        dialog.window?.setLayout(1000, 800)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+
+        // Initialize and assign variable
+        val editText: EditText = dialog.findViewById(R.id.edit_text)
+        val listView: ListView = dialog.findViewById(R.id.list_view)
+
+        val adapter: ArrayAdapter<Any?> = ArrayAdapter<Any?>(
+            this,
+            android.R.layout.simple_list_item_1,
+            resources.getStringArray(R.array.city_array)
+        )
+
+
+        listView.adapter = adapter
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+
+        listView.onItemClickListener =
+            OnItemClickListener { _, _, position, _ ->
+                addNewChip(adapter.getItem(position).toString())
+                dialog.dismiss()
+            }
+
+    }
+
+    private fun addNewChip(chipText: String) {
+        val chipGroup = binding.eventCityGroup
+        val chip =
+            layoutInflater.inflate(R.layout.item_chip_preference, chipGroup, false) as Chip
+        chip.text = chipText
+        chip.id = View.generateViewId()
+        chip.isClickable = false
+        chip.isCheckable = true
+        chip.isChecked = true
+        chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        chip.layoutParams = ChipGroup.LayoutParams(
+            ChipGroup.LayoutParams.WRAP_CONTENT,
+            ChipGroup.LayoutParams.WRAP_CONTENT
+        )
+        chip.isCloseIconVisible = true
+        chip.setCloseIconResource(R.drawable.baseline_close_24)
+        chip.setOnCloseIconClickListener {
+            listOfCity.remove(chipText)
+            chipGroup.removeView(chip)
+        }
+
+        listOfCity.add(chipText)
+        chipGroup.addView(chip, 0)
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    companion object {
+        const val USER_DATA = "user_data"
+        const val IMAGE_FILE = "image_file"
     }
 }
